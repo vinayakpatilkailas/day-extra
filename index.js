6729695c50d5edc6730492d1bb2f35e9e1bc0047
vinayakpatilkailas <105803130+vinayakpatilkailas@users.noreply.github.com>
2022-08-02 18:11:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+//Q1. What are different datatypes in Javascript?
+// Ans:
+//A) Primitive Datatype
+//B) Non-Primitive Datatype
+
+//A) Primitive Datatypes-
+//  1.Number
+//  2.Null
+//  3.Boolean
+//  4.Symbol
+//  5.String
+//  6.Big int
+//  7.Undefined
+//B) Non-Primitive Datatypes-
+//  1.Object
+//  2.Array
+
+//Q2. In how many ways can we declare a variable?
+//Ans:
+// 3ways- var,let,const
+    
+//     1)Let is an  Block scope 
+//     2)Let Cant be Re-declared 
+//     3)Let can be Modified Later
+//              &
+//     1)Const is an  Block scope 
+//     2)Const Cant be Re-declared 
+//     3)Const cant be Modified Later
+//              &
+//     1)var is an  Block scope 
+//     2)var Cant be Re-declared 
+//     3)var can be Modified Later
+
+// Q3. What is Hoisting?
+// Ans:
+// Hoisting :Hoisting is a Javascript Mechanism where variables and function declarations are moved to top of there scope before the code execution. Note:Only Declarations are hoisted not the initialization
+
+// Q4. What is an temporal dead zone ?
+// Ans:
+// It is the area of a block where a variable is inaccessible until the moment computer initializes it with a value
+// TDZ occurs when we declare a variable of Let and Const keyword.
+
+// Q5. What is the Difference between == & === Operator?
+// Ans:
+// == only checks the value 
+// === checks the both value and the data types
+
+// Q6. What are the Different types of Operators in Javascript?
+// Ans:
+//  1) Arithmetic Operators
+//  2) ComparisonOperators 
+//  3) Logical Operators
+//  4) Relational Operators
+//  5) Conditional Operator
+//  6) Bitwise Operator.
